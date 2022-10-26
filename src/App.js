10e1975c26d6a34ca6123b0f14e1be715162bebb
Arturo Barrantes <arturo.barrantesv@pucp.pe>
2022-10-26 00:54:22 -0500

    react app connected to wallet

---
+++ src/App.js
@@ -0,0 +1,51 @@
+import { clusterApiUrl } from '@solana/web3.js';
+import { WalletAdapterNetwork } from '@solana/wallet-adapter-base';
+import { PhantomWalletAdapter, } from '@solana/wallet-adapter-wallets';
+import { ConnectionProvider, WalletProvider } from '@solana/wallet-adapter-react';
+import { WalletModalProvider, WalletMultiButton } from '@solana/wallet-adapter-react-ui';
+
+import { useMemo } from 'react';
+
+require('./App.css');
+require('@solana/wallet-adapter-react-ui/styles.css');
+
+const App = () => {
+    return (
+        <Context>
+            <Content />
+        </Context>
+    );
+};
+export default App;
+
+const Context = ({ children }) => {
+    // The network can be set to 'devnet', 'testnet', or 'mainnet-beta'.
+    const network = WalletAdapterNetwork.Devnet;
+
+    // You can also provide a custom RPC endpoint.
+    const endpoint = useMemo(() => clusterApiUrl(network), [network]);
+
+    const wallets = useMemo(
+        () => [
+            new PhantomWalletAdapter(),
+            
+        ],
+        []
+    );
+
+    return (
+        <ConnectionProvider endpoint={endpoint}>
+            <WalletProvider wallets={wallets} autoConnect>
+                <WalletModalProvider>{children}</WalletModalProvider>
+            </WalletProvider>
+        </ConnectionProvider>
+    );
+};
+
+const Content = () => {
+    return (
+        <div className="App">
+            <WalletMultiButton />
+        </div>
+    );
+};
